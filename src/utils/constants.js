6ac31821279ab9e897f14c181a692586baf7ae47
Juliana Teixeira <52385385+ajuteixeira@users.noreply.github.com>
2024-09-14 18:42:23 -0300

    Adiciona comentarios ao constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
+// Arquivo definindo os tipos de operadores utilizados e suas conversões
 export const NEGATIVE = "∼";
 export const CONNECTIVES = [NEGATIVE, "∧", "∨", "→", "↔", "⊻"];
 export const PARSED_CONNECTIVES = ["(", ")", "!", "&&", "||", "^"];
