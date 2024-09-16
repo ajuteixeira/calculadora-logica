cac2305560fe3165e7483d0870edca116ebf4f66
ajuteixeira <teixeirajuuh@gmail.com>
2024-09-16 15:02:08 -0300

    ajuste visual do input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,15 @@
 export default function Input({ value, error = false }) {
   return (
-    <input
-      type="text"
+    <textarea
+      rows="2"
       value={value}
       name=""
       id=""
       readOnly={true}
       placeholder="expressão lógica"
-      className={`w-full h-[50px] p-3 outline-none bg-[#7927bd21] ${error ? 'border-2 border-rose-400' : ''}`}
-    />
+      className={`w-full p-3 outline-none overflow-hidden bg-[#7927bd21] ${
+        error ? "border-2 border-rose-400" : ""
+      }`}
+    ></textarea>
   );
 }
